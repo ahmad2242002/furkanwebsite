88b53b99cd42ae820f12ce9e88d332c7424bc15e
ahmad2242002 <f200125@cfd.nu.edu.pk>
2023-07-18 14:04:49 +0500

    updation 3

--- pages/index.js
+++ pages/index.js
@@ -28,16 +28,16 @@ export default function Home() {
         </div>
         </div>
    </div>
-   <div className=' grid grid-cols-2 h-auto w-100 lg:mx-44 md:mx-20 sm:mx-10 mx-5 my-20'>
-        <div className=' relative'>
+   <div className=' grid grid-cols-1 sm:grid-cols-2 h-auto w-100 lg:mx-44 md:mx-20 sm:mx-10 mx-5 my-20'>
+        <div className=' relative max-sm:h-72'>
         <Image src = {waiting} layout='fit' fill alt='image' className=' rounded-lg' quality={100}></Image>
         </div>
         <div className='z-10 pt-20 ps-10 space-y-8 flex flex-col items-center'>
-        <div className=" text-yellow-500 text-5xl font-semibold">International Education Consultants</div>
+        <div className=" text-yellow-500 md:text-5xl text-3xl font-semibold">International Education Consultants</div>
         <div className="text-black text-lg font-semibold">StudyUSA Expert is a renowned educational consultancy firm committed to assisting students in pursuing higher education in the United States. With personalized counseling, university selection, application assistance, standardized test preparation, visa support, scholarship and financial aid assistance, and pre-departure guidance, they provide comprehensive guidance and support at every stage of the academic journey. Their experienced team aims to empower students with the knowledge and resources needed to make informed decisions, ensuring a smooth and successful transition to world-class education and future success.</div>
             <div className="m-5">
-                <Link href='/about'><button className="rounded-full hover:scale-110 px-10 py-4 hover:drop-shadow-2xl text-base text-white font-bold  bg-blue-950 drop-shadow-md active:scale-90 transition duration-200">
-                <FontAwesomeIcon  icon={faPhone} className='me-2'/>  About Us</button></Link>
+                <button className="rounded-full hover:scale-110 px-5 sm:px-10 py-2 sm:py-4 hover:drop-shadow-2xl text-xs sm:text-base text-white font-bold  bg-blue-950 drop-shadow-md active:scale-90 transition duration-200">
+                <FontAwesomeIcon  icon={faPhone} className='sm:me-2'/>  Contact Us</button>
             </div>
             </div>
     </div>
